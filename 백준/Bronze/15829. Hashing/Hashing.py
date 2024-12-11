dd764c5cf7ff4224f0fb190522e46891953ca132
Lee Hyunji <105178646+hzi09@users.noreply.github.com>
2024-12-11 22:49:45 +0900

    [Bronze II] Title: Hashing, Time: 40 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,10 +1,8 @@
 n =  int(input())
 string = input()
 hashing = 0
-cnt = 0
 
-for i in string :
-    hashing += (ord(i)-96) * (31**cnt)
-    cnt += 1
+for i in range(n) :
+    hashing += (ord(string[i])-96) * (31**i)
 
-print(hashing)
+print(hashing % 1234567891)
