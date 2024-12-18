213b8e1b0e94a37e4d8407980e3b20d053503569
Lee Hyunji <105178646+hzi09@users.noreply.github.com>
2024-12-18 17:35:32 +0900

    Update 다항식 더하기.py

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.py
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.py
@@ -1,17 +1,17 @@
 def solution(polynomial):
-    x = 0 # x앞의 상수
-    a = 0 # 그냥 상수
+    a = 0 # x앞의 상수
+    b = 0 # 그냥 상수
     for i in polynomial.split(' + ') :
         if i.isdigit() :
-            a += int(i)
+            b += int(i)
         else :
             if i == "x" :
-                x += 1
+                a += 1
             else :
-                x += int(i[:-1])
-    if x == 0 :
-        return str(a)
-    elif x == 1 :
-        return f'x + {str(a)}' if a !=0 else 'x'
+                a += int(i[:-1])
+    if a == 0 :
+        return str(b)
+    elif a == 1 :
+        return f'x + {str(b)}' if b !=0 else 'x'
     else :
-        return f'{x}x + {a}' if a != 0 else f'{x}x'
+        return f'{a}x + {b}' if b != 0 else f'{a}x'
