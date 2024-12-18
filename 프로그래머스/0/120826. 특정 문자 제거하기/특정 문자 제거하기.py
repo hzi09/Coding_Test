7bc91bb0f48b8c3e918ca4335b7e71e82b65e861
Lee Hyunji <105178646+hzi09@users.noreply.github.com>
2024-12-18 09:02:10 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
@@ -1,6 +1,9 @@
 def solution(my_string, letter):
     str_l = list(my_string)
+    
     while letter in str_l :
         str_l.remove(letter)
+        
     answer = ''.join(str_l)
+    
     return answer
