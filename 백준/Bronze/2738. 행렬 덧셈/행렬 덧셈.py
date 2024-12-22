fe3f19306cb778fbf5afa45246caf08f7944837f
Lee Hyunji <105178646+hzi09@users.noreply.github.com>
2024-12-22 13:20:50 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -0,0 +1,7 @@
+n, m = map(int, input().split())
+
+a = [list(map(int, input().split())) for _ in range(n)]
+b = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(n) :
+    print(' '.join(str(a[i][j] + b[i][j]) for j in range(m)))
