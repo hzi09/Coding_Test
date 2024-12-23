edd6dc98cb98657d778cfd2423637ce721b7019b
Lee Hyunji <105178646+hzi09@users.noreply.github.com>
2024-12-23 17:37:33 +0900

    [level 0] Title: 등수 매기기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
@@ -1,10 +1,4 @@
 def solution(score):
-    avg_score = []
-    for i in score :
-        avg_score.append(sum(i)/2)
-        
+    avg_score = [sum(i)/2 for i in score]
     score_sort = sorted(avg_score, reverse=True)
-    
-    answer = [score_sort.index(i)+1 for i in avg_score]
-
-    return answer
+    return [score_sort.index(i)+1 for i in avg_score]
