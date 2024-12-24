211ea4c1550c49d55593aebe66ffd67d10021c29
Lee Hyunji <105178646+hzi09@users.noreply.github.com>
2024-12-24 10:23:30 +0900

    [level 0] Title: 평행, Time: 0.00 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120875. 평행/평행.py
@@ -0,0 +1,6 @@
+def solution(dots):
+    [[x1, y1], [x2, y2], [x3, y3], [x4, y4]]=dots
+    a1 = ((y1-y2)*(x3-x4) == (y3-y4)*(x1-x2))
+    a2 = ((y1-y3)*(x2-x4) == (y2-y4)*(x1-x3))
+    a3 = ((y1-y4)*(x2-x3) == (y2-y3)*(x1-x4))
+    return 1 if a1 or a2 or a3 else 0
