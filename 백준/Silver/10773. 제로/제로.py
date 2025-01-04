c2ace9e7c2a4d7350194a9b325315d6998b86b78
Lee Hyunji <105178646+hzi09@users.noreply.github.com>
2025-01-04 17:04:02 +0900

    [Silver IV] Title: 제로, Time: 2680 ms, Memory: 33192 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,12 @@
+k = int(input())
+
+answer =[]
+
+for _ in range(k) :
+    num = int(input())
+    if num == 0 :
+        del answer[-1]
+    else :
+        answer.append(num)
+
+print(sum(answer))
