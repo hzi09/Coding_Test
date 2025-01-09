39f571bc2df80737b3b6c3be02574e66b3e89fff
Lee Hyunji <105178646+hzi09@users.noreply.github.com>
2025-01-09 09:12:16 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -1,6 +1,2 @@
 def solution(my_string):
-    numbers = []
-    for i in my_string :
-        if i.isdigit() :
-            numbers.append(int(i))
-    return sorted(numbers)
+    return sorted([int(i) for i in my_string if i.isdigit()])
