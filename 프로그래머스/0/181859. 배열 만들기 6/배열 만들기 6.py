a5833e96c08094041434b208e5ecd8b3de0c0041
Lee Hyunji <105178646+hzi09@users.noreply.github.com>
2025-01-18 20:54:30 +0900

    [level 0] Title: 배열 만들기 6, Time: 242.96 ms, Memory: 17.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
@@ -0,0 +1,17 @@
+def solution(arr):
+    stk = []
+    i = 0
+    while i < len(arr) :
+        if len(stk) == 0 :
+            stk.append(arr[i])
+            i += 1
+        else :
+            if stk[-1] != arr[i] :
+                stk.append(arr[i])
+                i += 1
+            else :
+                del stk[-1]
+                i += 1
+    if len(stk) == 0 :
+        return [-1]
+    return stk
