cf8443ba0007da29e148fe5f33d6cec139ebaf2a
Lee Hyunji <105178646+hzi09@users.noreply.github.com>
2025-01-20 11:16:04 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.10 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.py
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.py
@@ -1,8 +1,9 @@
 def solution(arr):
-    if len(arr) > len(arr[0]) :
-        for i in range(len(arr)) :
-            arr[i] += [0] * (len(arr)-len(arr[-1]))
-    elif len(arr) < len(arr[0]) :
-        for i in range(len(arr[0])-len(arr)) :
-            arr.append([0] * len(arr[0]))
+    rows, cols = len(arr), len(arr[0])
+    if rows > cols:
+        for i in range(rows):
+            arr[i] += [0] * (rows - cols)
+    elif rows < cols:
+        for _ in range(cols - rows):
+            arr.append([0] * cols) 
     return arr
