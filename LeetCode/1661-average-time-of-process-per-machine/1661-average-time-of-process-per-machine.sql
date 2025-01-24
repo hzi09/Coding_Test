cba2d1de33ef334600a87818da6d9c0c4c0eb7dd
Lee Hyunji <105178646+hzi09@users.noreply.github.com>
2025-01-24 13:36:10 +0900

    Time: 365 ms (47.88%), Space: 0B (100%) - LeetHub

---
+++ LeetCode/1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -0,0 +1,5 @@
+SELECT s.machine_id, ROUND(AVG(e.timestamp-s.timestamp), 3) AS processing_time
+FROM Activity s JOIN Activity e ON s.machine_id = e.machine_id 
+                                AND s.process_id = e.process_id
+                                AND s.timestamp < e.timestamp
+GROUP BY machine_id
