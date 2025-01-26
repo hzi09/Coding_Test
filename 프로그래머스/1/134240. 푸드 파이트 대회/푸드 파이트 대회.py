5fc16a2bd565c205e53f00100f8a3b8f8a3ffe62
Lee Hyunji <105178646+hzi09@users.noreply.github.com>
2025-01-26 18:18:49 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -2,8 +2,5 @@ def solution(food):
     answer = ''
     for idx, f in enumerate(food) :
         if idx != 0 :
-            if f % 2 == 1 :
-                answer += str(idx)*((f-1)//2)
-            else :
-                answer += str(idx)*(f//2)         
+            answer += str(idx)*(f//2)      
     return answer + '0' + answer[::-1]
