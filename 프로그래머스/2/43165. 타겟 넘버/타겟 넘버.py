aaae4b7360755e0ba9b96aa07da18f0d9b5c6a22
Lee Hyunji <105178646+hzi09@users.noreply.github.com>
2025-02-04 10:24:21 +0900

    [level 2] Title: 타겟 넘버, Time: 448.33 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,9 @@
+def solution(numbers, target):
+    def dfs(index, total):
+
+        if index == len(numbers):
+            return 1 if total == target else 0
+        
+        return dfs(index + 1, total + numbers[index]) + dfs(index + 1, total - numbers[index])
+    
+    return dfs(0, 0)
