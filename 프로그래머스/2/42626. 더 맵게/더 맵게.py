f1c2112af85a07df394d72cbea416ebcfcc32bb2
Lee Hyunji <105178646+hzi09@users.noreply.github.com>
2025-02-06 09:46:10 +0900

    [level 2] Title: 더 맵게, Time: 1662.77 ms, Memory: 64.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,14 @@
+import heapq
+
+def solution(scoville, K) :
+    answer = 0
+    s = scoville[:]
+    heapq.heapify(s)
+    while s and s[0] < K:
+        try:
+            new_food = heapq.heappop(s) + heapq.heappop(s) * 2
+            heapq.heappush(s, new_food)
+            answer += 1
+        except:
+            return -1
+    return answer
