a4a7ac8bb554abe0e8aa5a799bc5dfe8e4f2dc42
Lee Hyunji <105178646+hzi09@users.noreply.github.com>
2025-02-14 09:32:17 +0900

    [level 2] Title: 소수 찾기, Time: 5.47 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -3,17 +3,23 @@ from itertools import permutations
 def solution(numbers):
     numbers = list(numbers)
     answer = []
+    
     for n in range(1, len(numbers)+1) :
         answer.extend(int(''.join(p)) for p in permutations(numbers, n))
+        
     cnt = 0
+    
     for i in set(answer) :
         if i < 2 :
             continue
+            
         is_prime = True
+        
         for j in range(2, int(i ** 0.5) + 1) :
             if i % j == 0:
                 is_prime = False
                 break
+                
         if is_prime :
             cnt += 1
             
