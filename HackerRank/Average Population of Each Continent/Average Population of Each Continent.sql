1d78b872b0a057243c3bffa9b972ab4a3f0df0a9
Lee Hyunji <hzi284914@gmail.com>
2025-03-25 01:28:36 +0900

    Weather Observation Station 20

--- HackerRank/Average Population of Each Continent/Average Population of Each Continent.sql
+++ HackerRank/Average Population of Each Continent/Average Population of Each Continent.sql
@@ -1,3 +1,13 @@
-SELECT CO.Continent, FLOOR(AVG(CI.Population))
-FROM CITY CI JOIN COUNTRY CO ON CI.CountryCode = CO.Code
-GROUP BY 1
+WITH Ordered AS (
+    SELECT LAT_N, ROW_NUMBER() OVER (ORDER BY LAT_N) AS row_num,
+                 COUNT(*) OVER () AS total_count
+    FROM STATION
+)
+SELECT ROUND(
+    CASE 
+        WHEN total_count % 2 = 1 
+        THEN (SELECT LAT_N FROM Ordered WHERE row_num = (total_count + 1) / 2)
+        ELSE (SELECT AVG(LAT_N) FROM Ordered WHERE row_num IN (total_count / 2, total_count / 2 + 1))
+        END, 4)
+FROM Ordered
+LIMIT 1;
