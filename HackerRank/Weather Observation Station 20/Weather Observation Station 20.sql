0b3f4052e99b65b1a43b13e114b048bbe7cafc41
Lee Hyunji <hzi284914@gmail.com>
2025-03-25 01:30:25 +0900

    edit code

--- HackerRank/Weather Observation Station 20/Weather Observation Station 20.sql
+++ HackerRank/Weather Observation Station 20/Weather Observation Station 20.sql
@@ -1,2 +1,13 @@
-SELECT ROUND(SQRT(POWER(MAX(LAT_N) - MIN(LAT_N), 2) + POWER(MAX(LONG_W) - MIN(LONG_W), 2)), 4)
-FROM STATION
+WITH Ordered AS (
+    SELECT LAT_N, ROW_NUMBER() OVER (ORDER BY LAT_N) AS row_num,
+                 COUNT(*) OVER () AS total_count
+    FROM STATION
+)
+SELECT ROUND(
+    CASE 
+        WHEN total_count % 2 = 1 
+        THEN (SELECT LAT_N FROM Ordered WHERE row_num = (total_count + 1) / 2)
+        ELSE (SELECT AVG(LAT_N) FROM Ordered WHERE row_num IN (total_count / 2, total_count / 2 + 1))
+        END, 4)
+FROM Ordered
+LIMIT 1;
