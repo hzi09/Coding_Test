fff5d748b10a34d92d33bf374a0e63ca9eac1bc4
Lee Hyunji <hzi284914@gmail.com>
2025-03-30 17:19:40 +0900

    Ollivander's Inventory

---
+++ HackerRank/Ollivander's Inventory/Ollivander's Inventory.sql
@@ -0,0 +1,9 @@
+SELECT id, age, coins_needed, power
+FROM Wands W JOIN Wands_Property WP ON W.code = WP.code
+AND coins_needed = (
+    SELECT MIN(coins_needed)
+    FROM Wands W2
+    JOIN Wands_Property WP2 ON W2.code = WP2.code
+    WHERE WP2.is_evil != 1 AND W2.power = W.power AND WP2.age = WP.age
+)
+ORDER BY 4 DESC, 2 DESC
