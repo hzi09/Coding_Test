89d56de4807b0105e4833db771482b585d49db9e
Lee Hyunji <105178646+hzi09@users.noreply.github.com>
2025-04-09 16:45:01 +0900

    [Bronze II] Title: 거스름돈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5585. 거스름돈/거스름돈.py
@@ -0,0 +1,13 @@
+n = 1000 - int(input())
+
+money = [500, 100, 50, 10, 5, 1]
+cnt = 0
+
+while n > 0:
+    for i in money:
+        if n >= i:
+            cnt += n // i
+            n %= i
+
+print(cnt)
+    
