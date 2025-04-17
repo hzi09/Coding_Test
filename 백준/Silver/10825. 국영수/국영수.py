96cd3c7f127a3359194b639c46e8e2898732bdff
Lee Hyunji <105178646+hzi09@users.noreply.github.com>
2025-04-17 19:02:22 +0900

    [Silver IV] Title: 국영수, Time: 2728 ms, Memory: 63432 KB -BaekjoonHub

---
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+students = []
+
+for _ in range(n):
+    name, korean, english, math = input().split()
+    students.append((name, int(korean), int(english), int(math)))
+
+students.sort(key=lambda x: (-x[1], x[2], -x[3], x[0]))
+
+for i in students:
+    print(i[0])
