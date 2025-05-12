d4355f81c2206d8866af498dfcbdc50759d2946f
Lee Hyunji <105178646+hzi09@users.noreply.github.com>
2025-05-12 18:31:29 +0900

    [Silver III] Title: 소수 구하기, Time: 296 ms, Memory: 47760 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,12 @@
+m, n = map(int, input().split())
+
+a = [False, False] + [True] * (n-1)
+
+for i in range(2, int(n**0.5) + 1):
+    if a[i]:
+        for j in range(i*i, n+1, i):
+            a[j] = False
+
+for i in range(m, n+1):
+    if a[i]:
+        print(i)
