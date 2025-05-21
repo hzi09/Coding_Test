79127795d6765f5fc34b827a93ec6fd084ef67ed
Lee Hyunji <105178646+hzi09@users.noreply.github.com>
2025-05-21 22:39:54 +0900

    [Silver IV] Title: 큐, Time: 68 ms, Memory: 34924 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,25 @@
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline())
+
+queue = deque()
+
+for i in range(n):
+    command = sys.stdin.readline().split()
+
+    if command[0] == "push":
+        queue.append(command[1])
+    elif command[0] == "pop":
+        if not queue:
+            print(-1)
+        else:
+            print(queue.popleft())
+    elif command[0] == 'size':
+        print(len(queue))
+    elif command[0] == 'empty':
+        print(1 if not queue else 0)
+    elif command[0] == 'front':
+        print(queue[0] if queue else -1)
+    elif command[0] == 'back':
+        print(queue[-1] if queue else -1)
