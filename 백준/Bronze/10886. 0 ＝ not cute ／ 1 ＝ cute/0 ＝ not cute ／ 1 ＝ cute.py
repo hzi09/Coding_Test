2dcc4ee55fe35e455a2e4474d051ace0a1f394af
Lee Hyunji <105178646+hzi09@users.noreply.github.com>
2025-12-01 13:20:07 +0900

    [Bronze III] Title: 0 = not cute / 1 = cute, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10886. 0 ＝ not cute ／ 1 ＝ cute/0 ＝ not cute ／ 1 ＝ cute.py
@@ -0,0 +1,15 @@
+n = int(input())
+cute = 0
+not_cute = 0
+
+for _ in range(n):
+    vote = int(input())
+    if vote == 1:
+        cute += 1
+    else :
+        not_cute += 1
+
+if cute > not_cute :
+    print("Junhee is cute!")
+else :
+    print("Junhee is not cute!")
